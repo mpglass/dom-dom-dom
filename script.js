aee3c6a4abf3bdd3c7cfdc46f9408751e35715d9
Michelle Glass <mglass@Michelles-MacBook-Air.local>
2020-04-20 11:44:01 -0500

    finished lab

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    
+
     let colorArray = ['red', 'orange', 'yellow', 'green', 'blue', 'purple'];
-    let number = 1
-    
+    let number = 0
+
     let container = document.createElement('main')
     container.className = 'container';
-    
+
 
     let row = document.createElement('section');
     row.className = 'row justify-content-center my-2';
@@ -23,28 +23,49 @@ document.addEventListener('DOMContentLoaded', (event) => {
     squareBlock.className = 'd-flex justify-content-center flex-wrap';
     container.appendChild(squareBlock);
 
-    
-    
+
+
     buttonAddSqr.addEventListener('click', function () {
         let squareDiv = document.createElement('div');
         squareDiv.className = 'square';
+        let squareText = document.createTextNode(number);
         squareDiv.id = number++
         squareBlock.append(squareDiv);
-        let squareText = document.createTextNode(number)
        
+
         squareDiv.addEventListener('click', function () {
-           let randomIndex = Math.floor(Math.random()*colorArray.length)
-           squareDiv.style.backgroundColor = colorArray[randomIndex]
+            let randomIndex = Math.floor(Math.random() * colorArray.length)
+            squareDiv.style.backgroundColor = colorArray[randomIndex]
         })
-        squareDiv.onmouseover = function (){
-            document.getElementById(squareDiv.id).innerHTML = (squareDiv.id);
+        squareDiv.onmouseover = function () {
+            squareDiv.appendChild(squareText);
         };
-        squareDiv.onmouseout = function (){
-            document.getElementById(squareDiv.id).innerHTML = ('')
+        squareDiv.onmouseout = function () {
+            squareDiv.removeChild(squareText)
         };
- 
-    })
 
+        squareDiv.addEventListener('dblclick', function () {
+            if (squareDiv.id % 2 == 0) {
+                if (squareDiv.nextSibling == null) {
+                    console.log('nope')
+                } else {
 
-});
+                    squareBlock.removeChild(squareDiv.nextSibling)
+                }
+            }
+            else {
+                if (squareDiv.previousSibling == null) {
+                    console.log('aint gonna happen')
+                } else {
+                    squareBlock.removeChild(squareDiv.previousSibling)
+
+                }
+
+            }
+
+        });
+        //end of add square button  
+    });
+    //end of DOM content load 
+})
 
